921f82724bdaa81171449a51627bdf8ec46bb313
Ashish Mamgain <ashish.mamgain07@gmail.com>
2026-08-27 08:07:38 +0530

    DSA Code

--- src/com/learn/coding/dsa/array/SecondLargest.java
+++ src/com/learn/coding/dsa/array/SecondLargest.java
@@ -2,36 +2,36 @@ package com.learn.coding.dsa.array;
 
 public class SecondLargest {
 
-	public static void main(String[] args) {
-		int[] nums = { 10, 20, 4, 45, 99 };
-		System.out.println("Second Largest: " + findSecondLargest(nums));
-		
-	}
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 4, 45, 99};
+		findSecondLargest(nums);
 
-	private static int findSecondLargest(int[] nums) {
-		
-		if(nums.length<2) {			
-			throw new IllegalArgumentException("Illegal Argument Exception");
-		}
-		
-		int largest= Integer.MIN_VALUE;
-		int secondLargest= Integer.MIN_VALUE;
-		
-		for(int i=0; i<nums.length;i++) {			
-			if(nums[i]>largest) {
-				secondLargest=largest;
-				largest =nums[i];
-			}
-			else if(nums[i]>secondLargest &&  nums[i]<largest) {
-				secondLargest =nums[i];
-			}		
-		}
-		
-		if(secondLargest == Integer.MIN_VALUE) {
-			throw new RuntimeException("No second largest");
-		}
-		
-		return secondLargest;
-	}
+    }
+
+    private static void findSecondLargest(int[] nums) {
+
+        if (nums.length < 2) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+
+        int firstLargest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (Integer num : nums) {
+            if (num > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = num;
+            } else if (num > secondLargest && num < firstLargest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No valid second largest element");
+        } else {
+            System.out.println("First Largest : " + firstLargest);
+            System.out.println("Second Largest : " + secondLargest);
+        }
+    }
 
 }
