8a2471b1b47ac19825063479b785bfbe0c6d91b2
Ashish Mamgain <ashish.mamgain07@gmail.com>
2026-08-27 07:46:56 +0530

    DSA Code

--- src/com/learn/coding/dsa/array/MaximumSubArraySum.java
+++ src/com/learn/coding/dsa/array/MaximumSubArraySum.java
@@ -4,9 +4,54 @@ public class MaximumSubArraySum {
 
     public static void main(String[] args) {
         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        getMaxSubarray(arr);
-        getMaxSumSubarray(arr);
+        maxSubArray(arr);//tuf easy
+        // getMaxSubarray(arr);
+        //getMaxSumSubarray(arr);
+
     }
+
+    // Function to find maximum sum of subarrays
+    public static void maxSubArray(int[] nums) {
+
+        // Maximum sum
+        long maxSum = Long.MIN_VALUE;
+
+        // Current sum of subarray
+        long sum = 0;
+
+        //IF subarray needed
+        int startIndex = 0;
+        int endIndex = 0;
+        int tempIndex = 0;
+
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+
+            // Add current element to the sum
+            sum += nums[i];
+
+            // Update maxi if current sum is greater
+            if (sum > maxSum) {
+                maxSum = sum;
+                startIndex = tempIndex;
+                endIndex = i;
+            }
+
+            // Reset sum to 0 if it becomes negative
+            if (sum < 0) {
+                sum = 0;
+                tempIndex = i + 1; // start of next segment
+            }
+        }
+
+        // Return the maximum subarray sum found
+        System.out.print("Sum is : " + maxSum + "\nSubarray : [");
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.print("" + nums[i] + (i < endIndex ? ", " : ""));
+        }
+        System.out.print("]");
+    }
+
 
     private static void getMaxSubarray(int[] arr) {
 
